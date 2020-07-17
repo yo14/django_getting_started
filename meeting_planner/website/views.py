0a65b7319edc2719085d9c4e7240be5b6775ce52
yo14 <ifliandry11@gmail.com>
2020-07-17 15:14:33 +0700

    Creating another pages

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,5 +1,14 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def welcome(request):
     return HttpResponse("Welcome to the Meeting Planner!")
+
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
+
+def about(request):
+    return HttpResponse("<h1>About me</h1><p>I am a remote worker programmer</p>")
