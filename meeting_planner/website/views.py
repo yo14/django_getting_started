365f6a2d53ab73789e77597cabc724d8a44b6eef
yo14 <ifliandry11@gmail.com>
2020-07-18 21:59:34 +0700

    Beginning: start using templates

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def welcome(request):
-    return HttpResponse("Welcome to the Meeting Planner!")
+    return render(request, "website/welcome.html",
+                  {"message":"This data was sent from the view to the template."})
 
 def date(request):
     return HttpResponse("This page was served at " + str(datetime.now()))
