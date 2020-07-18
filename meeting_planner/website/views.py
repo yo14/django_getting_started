53e6500344eaaaf1a11ffd1e06beb5c592c4fcb1
yo14 <ifliandry11@gmail.com>
2020-07-18 08:16:21 +0700

    Creating new app meeting_planner with class migrate, model and table

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -6,7 +6,6 @@ from django.http import HttpResponse
 def welcome(request):
     return HttpResponse("Welcome to the Meeting Planner!")
 
-
 def date(request):
     return HttpResponse("This page was served at " + str(datetime.now()))
 
