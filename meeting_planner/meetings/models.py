6e961ebe444f8f681f41d0285e5a579c5b5e7984
yo14 <ifliandry11@gmail.com>
2020-07-18 09:50:45 +0700

    Completed meeting app including add its model to admin

--- meeting_planner/meetings/models.py
+++ meeting_planner/meetings/models.py
@@ -1,5 +1,12 @@
+from datetime import time
 from django.db import models
 
 class Meeting(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
+    start_time = models.TimeField(default=time(9))
+    duration = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.title} at {self.start_time} on {self.date}"
+
