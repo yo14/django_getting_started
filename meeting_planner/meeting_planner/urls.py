bd1a789a16d8f9800d1e15002169b350a6cbb3fd
yo14 <ifliandry11@gmail.com>
2020-07-19 05:02:29 +0700

    Adding detail page for meeting

--- meeting_planner/meeting_planner/urls.py
+++ meeting_planner/meeting_planner/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from website.views import welcome, date, about
+from meetings.views import detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', welcome),
     path('date', date),
     path('about', about),
+    path('meetings/<int:id>', detail)
 ]
 
--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from meetings.models import Meeting
+
+def detail(request, id):
+    meeting = Meeting.objects.get(pk=id)
+    return render(request, "meetings/detail.html", {"meeting": meeting})
+
+
