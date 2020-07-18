4055e7adf5fc9cf6ac5018dd879e35df533e4db1
yo14 <ifliandry11@gmail.com>
2020-07-18 22:30:48 +0700

    Using Model Template View for welcome page

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -3,9 +3,12 @@ from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from meetings.models import Meeting
+
+
 def welcome(request):
     return render(request, "website/welcome.html",
-                  {"message":"This data was sent from the view to the template."})
+                  {"num_meetings": Meeting.objects.count()})
 
 def date(request):
     return HttpResponse("This page was served at " + str(datetime.now()))
