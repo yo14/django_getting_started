9670404ad2b737cccdbf76772563559f1003b6bb
yo14 <ifliandry11@gmail.com>
2020-07-19 07:16:55 +0700

    Setting 404 page

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 
 from meetings.models import Meeting
 
 def detail(request, id):
-    meeting = Meeting.objects.get(pk=id)
+    meeting = get_list_or_404(Meeting, pk=id)
     return render(request, "meetings/detail.html", {"meeting": meeting})
-
-
