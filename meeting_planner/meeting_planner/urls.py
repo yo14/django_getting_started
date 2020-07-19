7f2183f61d9b1e53c58862e2c6fec59b96fa351b
yo14 <ifliandry11@gmail.com>
2020-07-19 16:00:48 +0700

    Adding static content css and image

--- meeting_planner/meeting_planner/urls.py
+++ meeting_planner/meeting_planner/urls.py
@@ -14,16 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from website.views import welcome, date, about
-from meetings.views import detail, rooms_list
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', welcome),
     path('date', date),
     path('about', about),
-    path('meetings/<int:id>', detail),
-    path('rooms', rooms_list, name="rooms"),
+    path('meetings/', include('meetings.urls')),
 ]
 
